0e4ed02d23fc45f9d3c4d69996c8b1cdb5378377
LEONARDOPC\User <leonardasbutkus@gmail.com>
2023-10-09 13:54:18 +0300

    Added post deletion function

--- src/components/SinglePost.tsx
+++ src/components/SinglePost.tsx
@@ -1,15 +1,17 @@
-import {IncomingDataTypes, UserTypes} from "../types";
+import {IncomingDataTypes, ReduxTypes, UserTypes} from "../types";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCommentDots, faThumbsDown, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
+import {faCommentDots, faThumbsDown, faThumbsUp, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
 import {apiService} from "../api/api";
 import React, {useState} from "react";
 import {NavigateFunction, useNavigate} from "react-router-dom";
 import {emitPostInteraction} from "../api/sockets.ts";
 import useFormatTime from "../hooks/useFormatTime.ts";
 import useInteractionCheck from "../hooks/useInteractionCheck.ts";
+import {useSelector} from "react-redux";
 
 const SinglePost = ({post}: {post: UserTypes.Post}) => {
     const nav: NavigateFunction = useNavigate()
+    const user: UserTypes.User = useSelector((state: ReduxTypes.ReduxStates) => state.states.user)
     const [error, setError] = useState<string>('')
     async function likePost (): Promise<void> {
         const response:IncomingDataTypes.DefaultResponse = await apiService.likePost(post._id)
@@ -20,6 +22,16 @@ const SinglePost = ({post}: {post: UserTypes.Post}) => {
     }
     async function dislikePost (): Promise<void> {
         const response:IncomingDataTypes.DefaultResponse = await apiService.dislikePost(post._id)
+        if (!response.error){
+            emitPostInteraction()
+            setError('')
+        }else{
+            setError(response.message)
+        }
+    }
+
+    async function deletePost (): Promise<void> {
+        const response:IncomingDataTypes.DefaultResponse = await apiService.deletePost(post._id)
         if (!response.error){
             emitPostInteraction()
             setError('')
@@ -44,7 +56,7 @@ const SinglePost = ({post}: {post: UserTypes.Post}) => {
                 <div className='d-flex align-items-center gap-4'>
                     <div className='reaction-and-count'>
                         <div>{post.likes.length}</div>
-                        <div className='reaction' onClick={async (e: React.MouseEvent<HTMLDivElement>) => {
+                        <div className='reaction' onClick={async (e: React.MouseEvent<HTMLDivElement>): Promise<void> => {
                             e.stopPropagation()
                             await likePost()
                         }}>
@@ -53,7 +65,7 @@ const SinglePost = ({post}: {post: UserTypes.Post}) => {
                     </div>
                     <div className='reaction-and-count'>
                         <div>{post.dislikes.length}</div>
-                        <div className='reaction' onClick={async (e: React.MouseEvent<HTMLDivElement>) => {
+                        <div className='reaction' onClick={async (e: React.MouseEvent<HTMLDivElement>): Promise<void> => {
                             e.stopPropagation()
                             await dislikePost()
                         }}>
@@ -66,6 +78,13 @@ const SinglePost = ({post}: {post: UserTypes.Post}) => {
                             <FontAwesomeIcon icon={faCommentDots}/>
                         </div>
                     </div>
+                    {post.username === user.username &&
+                        <div className='delete-post-button' onClick={async (e: React.MouseEvent<HTMLDivElement>): Promise<void> => {
+                            e.stopPropagation()
+                            await deletePost()
+                        }}><FontAwesomeIcon icon={faTrashAlt} />
+                        </div>
+                    }
                 </div>
             </div>
             <div>{error}</div>
