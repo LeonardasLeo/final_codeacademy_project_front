a75b652d6f779012053a3d88678a9a7d448fbf0a
LEONARDOPC\User <leonardasbutkus@gmail.com>
2023-10-27 17:52:36 +0300

    fixed custom dropdown z-index

--- config.ts
+++ config.ts
@@ -1,11 +1,10 @@
-import {Config} from "./src/types";
-
+import { Config } from './src/types';
 
 const config: Config = {
-    serverRoute: 'http://78.62.12.216:3001',
-    token: () => {
-        return localStorage.getItem('token') || sessionStorage.getItem('token')
-    }
-}
+  serverRoute: 'http://localhost:3001',
+  token: () => {
+    return localStorage.getItem('token') || sessionStorage.getItem('token');
+  },
+};
 
-export default config
+export default config;
